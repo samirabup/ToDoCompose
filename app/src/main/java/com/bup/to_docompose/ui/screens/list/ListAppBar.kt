934ffe9dc964500352049f28b81ae3a2bc55b26e
samia-bup <say2samirabup@gmail.com>
2023-09-11 20:20:45 -0500

    searchAppBar done

--- app/src/main/java/com/bup/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/bup/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,8 +1,14 @@
 package com.bup.to_docompose.ui.screens.list
 
+import android.view.animation.AlphaAnimation
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -10,7 +16,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -25,16 +34,29 @@ import com.bup.to_docompose.ui.theme.topAppBarBackgroundColor
 import com.bup.to_docompose.ui.theme.topAppBarContentColor
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.core.graphics.alpha
 import com.bup.to_docompose.components.PriorityItem
 import com.bup.to_docompose.ui.theme.LARGE_PADDING
+import com.bup.to_docompose.ui.theme.TOP_APP_BAR_HEIGHT
 import com.bup.to_docompose.ui.theme.Typography
 
 @Composable
 fun ListAppBar(){
-    DefaultListAppBar(
-        onSearchClicked = {},
-        onSortClicked = {},
-        onDeleteClicked = {}
+//    DefaultListAppBar(
+//        onSearchClicked = {},
+//        onSortClicked = {},
+//        onDeleteClicked = {}
+//    )
+    SearchAppBar(
+        text = "Search" ,
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
     )
 }
 
@@ -166,6 +188,83 @@ fun DeleteAllAction(
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        color = MaterialTheme.colorScheme.topAppBarBackgroundColor
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = text ,
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder = {
+                Text(
+                    modifier = Modifier
+                        .alpha(alpha = 0.1f),
+                    text = "Search",
+                    color = Color.Black
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.topAppBarContentColor,
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier
+                        .alpha(alpha = 0.5f),
+                    onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                        tint = MaterialTheme.colorScheme.topAppBarContentColor
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        onCloseClicked()
+                    }
+                ) {
+                    Icon(imageVector = Icons.Filled.Close ,
+                        contentDescription = "Close Icon",
+                        tint = MaterialTheme.colorScheme.topAppBarContentColor
+                    )
+                    
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colorScheme.topAppBarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+}
 @Composable
 @Preview
 private fun DefaultListAppBarPreview(){
@@ -173,5 +272,16 @@ private fun DefaultListAppBarPreview(){
         onSearchClicked = {},
         onSortClicked = {},
         onDeleteClicked = {}
+    )
+}
+
+@Composable
+@Preview
+private fun SearchAppBarPreview(){
+    SearchAppBar(
+        text = "Search" ,
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
     )
 }
