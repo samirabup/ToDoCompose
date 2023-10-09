cadbd321b63b96d529930227e716e0068678d4bb
samia-bup <say2samirabup@gmail.com>
2023-10-09 12:13:04 -0500

    using custom theme and color

--- app/src/main/java/com/bup/to_docompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/bup/to_docompose/ui/screens/list/ListContent.kt
@@ -1,17 +1,23 @@
 package com.bup.to_docompose.ui.screens.list
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,13 +25,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bup.to_docompose.R
 import com.bup.to_docompose.data.models.Priority
 import com.bup.to_docompose.data.models.ToDoTask
+import com.bup.to_docompose.ui.theme.HighPriorityColor
+import com.bup.to_docompose.ui.theme.LARGEST_PADDING
 import com.bup.to_docompose.ui.theme.LARGE_PADDING
 import com.bup.to_docompose.ui.theme.PRIORITY_INDICATOR_SIZE
 import com.bup.to_docompose.ui.theme.TASK_ITEM_ELEVATION
@@ -111,11 +123,40 @@ fun DisplayTasks(
                 task.id
             }
         ){ task ->
-            TaskItem(
-                toDoTask = task,
-                navigationToTaskScreen = navigationToTaskScreen
-            )
+//            val dismissState = rememberDismissState()
+//            val degrees by animateFloatAsState(
+//                if (dismissState.targetValue == DismissValue.Default)
+//                    0f
+//            else
+//                -45f
+//            )
+//            SwipeToDismiss(
+//                state = dismissState,
+//                directions = setOf(DismissDirection.EndToStart),
+//                dismissThresholds = { FractionalThreshold(fraction = 0.2f)},
+//                background = { RedBackground(degrees = degrees)},
+//                dismissContent = {
+//
+//                }
+//            )
         }
+    }
+}
+
+@Composable
+fun RedBackground(degrees: Float){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(HighPriorityColor)
+        .padding(horizontal = LARGEST_PADDING),
+    contentAlignment = Alignment.CenterEnd
+    ){
+        Icon(
+            modifier = Modifier.rotate(degrees = degrees),
+            imageVector = Icons.Filled.Delete ,
+            contentDescription = stringResource(id = R.string.delete_icon),
+            tint = Color.White
+        )
     }
 }
 
--- app/src/main/java/com/bup/to_docompose/ui/theme/Color.kt
+++ app/src/main/java/com/bup/to_docompose/ui/theme/Color.kt
@@ -1,24 +1,84 @@
 package com.bup.to_docompose.ui.theme
 
-import android.provider.CalendarContract
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFF6C1DFF)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFE8DDFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF21005E)
+val md_theme_light_secondary = Color(0xFF345CA8)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFD8E2FF)
+val md_theme_light_onSecondaryContainer = Color(0xFF001A43)
+val md_theme_light_tertiary = Color(0xFF984062)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFD9E3)
+val md_theme_light_onTertiaryContainer = Color(0xFF3E001E)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF3E0021)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF3E0021)
+val md_theme_light_surfaceVariant = Color(0xFFE6E0EC)
+val md_theme_light_onSurfaceVariant = Color(0xFF48454E)
+val md_theme_light_outline = Color(0xFF79757F)
+val md_theme_light_inverseOnSurface = Color(0xFFFFECF0)
+val md_theme_light_inverseSurface = Color(0xFF5D1137)
+val md_theme_light_inversePrimary = Color(0xFFCEBDFF)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF6C1DFF)
+val md_theme_light_outlineVariant = Color(0xFFCAC4CF)
+val md_theme_light_scrim = Color(0xFF000000)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_dark_primary = Color(0xFFCEBDFF)
+val md_theme_dark_onPrimary = Color(0xFF390094)
+val md_theme_dark_primaryContainer = Color(0xFF5200CE)
+val md_theme_dark_onPrimaryContainer = Color(0xFFE8DDFF)
+val md_theme_dark_secondary = Color(0xFFAEC6FF)
+val md_theme_dark_onSecondary = Color(0xFF002E6B)
+val md_theme_dark_secondaryContainer = Color(0xFF14448F)
+val md_theme_dark_onSecondaryContainer = Color(0xFFD8E2FF)
+val md_theme_dark_tertiary = Color(0xFFFFB0C9)
+val md_theme_dark_onTertiary = Color(0xFF5E1134)
+val md_theme_dark_tertiaryContainer = Color(0xFF7A294B)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFD9E3)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF3E0021)
+val md_theme_dark_onBackground = Color(0xFFFFD9E4)
+val md_theme_dark_surface = Color(0xFF3E0021)
+val md_theme_dark_onSurface = Color(0xFFFFD9E4)
+val md_theme_dark_surfaceVariant = Color(0xFF48454E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCAC4CF)
+val md_theme_dark_outline = Color(0xFF938F99)
+val md_theme_dark_inverseOnSurface = Color(0xFF3E0021)
+val md_theme_dark_inverseSurface = Color(0xFFFFD9E4)
+val md_theme_dark_inversePrimary = Color(0xFF6C1DFF)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFCEBDFF)
+val md_theme_dark_outlineVariant = Color(0xFF48454E)
+val md_theme_dark_scrim = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val seed = Color(0xFF6C1DFF)
+val CustomColor1 = Color(0xFF477B73)
+val light_CustomColor1 = Color(0xFF006A60)
+val light_onCustomColor1 = Color(0xFFFFFFFF)
+val light_CustomColor1Container = Color(0xFF74F8E6)
+val light_onCustomColor1Container = Color(0xFF00201C)
+val dark_CustomColor1 = Color(0xFF53DBCA)
+val dark_onCustomColor1 = Color(0xFF003731)
+val dark_CustomColor1Container = Color(0xFF005048)
+val dark_onCustomColor1Container = Color(0xFF74F8E6)
+
 
 val LightGray = Color(0xFFFCFCFC)
 val MediumGray = Color(0xFF9C9C9C)
@@ -37,7 +97,7 @@ val ColorScheme.taskItemBackgroundColor: Color
     get() = if (!isSystemInDarkTheme()) Color.White else Color.DarkGray
 val ColorScheme.fabBackgroundColor: Color
     @Composable
-    get() = if (!isSystemInDarkTheme()) Teal200 else Purple700
+    get() = if (!isSystemInDarkTheme()) Color(0xFF03DAC5) else Color(0xFF3700B3)
 
 val ColorScheme.topAppBarContentColor: Color
     @Composable
