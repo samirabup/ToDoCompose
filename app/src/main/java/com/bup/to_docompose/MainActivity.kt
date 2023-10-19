821630b3bee566cbbb8b36ca0feefdf3baf38b26
samia-bup <say2samirabup@gmail.com>
2023-10-19 13:56:23 -0500

    project  primarily done

--- app/src/main/java/com/bup/to_docompose/MainActivity.kt
+++ app/src/main/java/com/bup/to_docompose/MainActivity.kt
@@ -5,20 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.bup.to_docompose.navigation.SetupNavigation
 import com.bup.to_docompose.ui.theme.ToDoComposeTheme
 import com.bup.to_docompose.ui.viewmodels.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
+@ExperimentalMaterial3Api
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     
     private lateinit var navController: NavHostController
     private val sharedViewModel: SharedViewModel by viewModels()
-    @ExperimentalMaterial3Api
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
