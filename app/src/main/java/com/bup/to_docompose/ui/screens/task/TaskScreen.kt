0a5794f504f831b0a2da64cc7594ad9e85102d6f
samia-bup <say2samirabup@gmail.com>
2023-10-20 12:49:14 -0500

    project  update 1

--- app/src/main/java/com/bup/to_docompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/bup/to_docompose/ui/screens/task/TaskScreen.kt
@@ -3,10 +3,16 @@ package com.bup.to_docompose.ui.screens.task
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
 import com.bup.to_docompose.data.models.Priority
 import com.bup.to_docompose.data.models.ToDoTask
@@ -26,6 +32,8 @@ fun TaskScreen(
     val priority: Priority by sharedViewModel.priority
 
     val context = LocalContext.current
+    
+    BackHandler(onBackPressed = {navigateToListScreen(Action.NO_ACTION)})
 
     Scaffold(
         topBar = {
@@ -71,4 +79,30 @@ fun displayToast(context: Context) {
         Toast.LENGTH_SHORT
     ).show()
 
+}
+
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+){
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+
+    val backCallback = remember {
+        object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backDispatcher ){
+        backDispatcher?.addCallback(backCallback)
+
+        onDispose {
+            backCallback.remove()
+        }
+    }
+    
 }
